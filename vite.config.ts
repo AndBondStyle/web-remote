95c576c780c133186e16a28ea7ab3caa73d707e5
Andrew Bond <andbondstyle@gmail.com>
2025-05-18 20:26:21 +0300

    Github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,11 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import Components from "unplugin-vue-components/vite"
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers"
-// import vueDevTools from "vite-plugin-vue-devtools"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    // vueDevTools(),
-    Components({
-      resolvers: [AntDesignVueResolver({ importStyle: false })],
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  server: {
-    allowedHosts: true,
-  },
+  plugins: [vue(), Components({ resolvers: [AntDesignVueResolver({ importStyle: false })] })],
+  resolve: { alias: { "@": fileURLToPath(new URL("./src", import.meta.url)) } },
+  server: { allowedHosts: true },
+  base: "/web-remote/",
 })
